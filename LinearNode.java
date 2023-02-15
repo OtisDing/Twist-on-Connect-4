dbcb0eea14f508fa7d8f88e25ce5a3d9661d9771
OtisDing <93888172+OtisDing@users.noreply.github.com>
2023-02-14 22:28:39 -0500

    Add files via upload

---
+++ LinearNode.java
@@ -0,0 +1,43 @@
+//Written by Otis Ding
+//Student ID: 251220811
+
+
+public class LinearNode<E>
+{
+    private LinearNode<E> next;
+    private E element;
+    
+    public LinearNode()
+    {
+        next = null;
+        element = null;
+    }
+    
+    public LinearNode (E elem)
+    {
+        next = null;
+        element = elem;
+    }
+    
+    public LinearNode<E> getNext()
+    {
+        return next;
+    }
+
+    public void setNext (LinearNode<E> node)
+    {
+        next = node;
+    }
+    
+
+    public E getElement()
+    {
+        return element;
+    }
+    
+
+    public void setElement (E elem)
+    {
+        element = elem;
+    }
+}
